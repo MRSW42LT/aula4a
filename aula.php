82d42388b004708994bf4b8f510b57f2c60200cb
denisborbasianhaki <81716552+denisborbasianhaki@users.noreply.github.com>
2022-03-16 07:53:57 -0300

    vbnvbnvbn

--- aula.php
+++ aula.php
@@ -35,6 +35,6 @@
 <?php 
     $a = 2;
     $b = 4;
-    echo $a+3*4+5*$b;
-    echo ($a + 3)*4+(5*$b);
+    echo $a+3*4+5*$b."<br>";       //resultado = 34
+    echo ($a + 3)*4+(5*$b); //resultado = 40
 ?>
